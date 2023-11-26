01ca16100ac6eb72441174e4bb01c29d3948df41
AbedAlRahmanMneimneh <aboudi_mneimneh@outlook.com>
2023-11-26 12:30:41 +0200

    Creating the project having some Scenes, Controllers (untouched yet, still on default), icons, ...

--- src/main/java/com/example/libraryint/HelloApplication.java
+++ src/main/java/com/example/libraryint/HelloApplication.java
@@ -14,7 +14,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("\\com\\example\\libraryint\\scenes\\hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
