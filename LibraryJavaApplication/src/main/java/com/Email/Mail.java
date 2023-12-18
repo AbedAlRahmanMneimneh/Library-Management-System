8bfe2312afce35ee3eb9ddae39aa4876f692aa37
MahmoudKebbi <mahmoud.kebbi180@gmail.com>
2023-12-18 18:46:18 +0200

    Update Mail.java

--- LibraryJavaApplication/src/main/java/com/Email/Mail.java
+++ LibraryJavaApplication/src/main/java/com/Email/Mail.java
@@ -8,26 +8,17 @@ import javax.mail.Transport;
 
 
 public class Mail {
-
-    public static void main(String[] args) throws MessagingException {
-        Properties prop = new Properties();
-        prop.put("mail.smtp.auth", true);
-        prop.put("mail.smtp.starttls.enable", "true");
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "25");
-        Session session = Session.getDefaultInstance(prop, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("apolloscriptlibrary@gmail.com", "tdluxehshmqybktc");
-            }
-        });
+    private String emailFrom = "apolloscriptlibrary@gmail.com";
+    private String appPassword = "tdluxehshmqybktc";
+    public  void sendmail(String emailTo, String Subject, String Contents) throws MessagingException {
+        Session session = createSessionAuth();
         Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress("apolloscriptlibrary@gmail.com"));
+        message.setFrom(new InternetAddress(emailFrom));
         message.setRecipients(
-                Message.RecipientType.TO, InternetAddress.parse("jbaradei2003@gmail.com"));
-        message.setSubject("Welcome to Apollo");
+                Message.RecipientType.TO, InternetAddress.parse(emailTo));
+        message.setSubject(Subject);
 
-        String msg = "Ayre bl LAU";
+        String msg = Contents;
 
         MimeBodyPart mimeBodyPart = new MimeBodyPart();
         mimeBodyPart.setContent(msg, "text/html; charset=utf-8");
@@ -38,5 +29,22 @@ public class Mail {
         message.setContent(multipart);
 
         Transport.send(message);
+    }
+    private Properties setProperties(){
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "25");
+        return prop;
+    }
+    private Session createSessionAuth(){
+        Session session = Session.getDefaultInstance(setProperties(), new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(emailFrom, appPassword);
+            }
+        });
+        return session;
     }
 }
