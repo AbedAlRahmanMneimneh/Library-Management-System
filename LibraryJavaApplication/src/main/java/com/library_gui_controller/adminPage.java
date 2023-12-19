b6e70c5480a946463feaa3874f145e86879ec977
AbedAlRahmanMneimneh <aboudi_mneimneh@outlook.com>
2023-12-19 21:06:59 +0200

    DID MANY THINGS THAT I CAN'T EVEN COUNT !!!!!!

--- LibraryJavaApplication/src/main/java/com/library_gui_controller/adminPage.java
+++ LibraryJavaApplication/src/main/java/com/library_gui_controller/adminPage.java
@@ -1,58 +1,99 @@
 package com.library_gui_controller;
 
-
 import com.library_entity_controllers.Book;
 import com.library_entity_controllers.Client;
+import com.library_entity_controllers.RoomsReservation;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class adminPage implements Initializable {
 
-
-
     @FXML
-    private TableView<Book> tableBooks;
-
-    @FXML
-    private TableColumn<Book, Integer> col_ISBN;
-
-    @FXML
-    private TableColumn<Book, String> col_author;
-
-    @FXML
-    private TableColumn<Book, Integer> col_edition;
-
-    @FXML
-    private TableColumn<Book, String> col_genre1;
-
-    @FXML
-    private TableColumn<Book, String> col_pubdate;
-
-    @FXML
-    private TableColumn<Book, String> col_publisher;
-
-    @FXML
-    private TableColumn<Book, String> col_title;
-
-    @FXML
-    private TextField AuthorTF;
+    private TextField BookCopyNumberTF;
 
     @FXML
     private TextField ISBNTF;
 
     @FXML
+    private TextField ISBNcopyTF;
+
+    @FXML
+    private TextField UserID;
+
+    @FXML
+    private Button addBookCopy;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private TextField authorTF;
+
+    @FXML
+    private TableColumn<Book, Integer> col_ISBN;
+
+    @FXML
+    private TableColumn<RoomsReservation, Integer> col_RoomID;
+
+    @FXML
+    private TableColumn<Book, String> col_author;
+
+    @FXML
+    private TableColumn<Book, Integer> col_copyNo;
+
+    @FXML
+    private TableColumn<RoomsReservation, String> col_reservationDateTime;
+
+    @FXML
+    private TableColumn<RoomsReservation, String> col_reservationEndTime;
+
+    @FXML
+    private TableColumn<RoomsReservation, Integer> col_reservationID;
+
+    @FXML
+    private TableColumn<Book, String> col_title;
+
+    @FXML
+    private TableColumn<RoomsReservation, Integer> col_userID;
+
+    @FXML
+    private TextArea content;
+
+    @FXML
+    private TextField copyNumber;
+
+    @FXML
+    private Button deleteBookCopy;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private TextField descriptionTF;
+
+    @FXML
     private TextField editionTF;
 
     @FXML
+    private Label fileNameLabel;
+
+    @FXML
     private TextField genreTF;
+
+    @FXML
+    private TextField numberOfPages;
 
     @FXML
     private TextField pubDateTF;
@@ -61,68 +102,126 @@ public class adminPage implements Initializable {
     private TextField publisherTF;
 
     @FXML
+    private Button rentBook;
+
+    @FXML
+    private TextField reservationEndTime;
+
+    @FXML
+    private TextField reservationTime;
+
+    @FXML
+    private Button reserveButton;
+
+    @FXML
+    private Button returnBook;
+
+    @FXML
+    private Button sendEmail;
+
+    @FXML
+    private Button sendReminder;
+
+    @FXML
+    private TextField studyRoomID;
+
+    @FXML
+    private TextField subject;
+
+    @FXML
+    private TableView<Book> tableBooks;
+
+    @FXML
+    private TableView<RoomsReservation> tableReservations;
+
+    @FXML
     private TextField titleTF;
 
     @FXML
-    private Button addButton;
+    private TextField to;
 
     @FXML
     private Button updateButton;
 
     @FXML
-    private Button deleteButton;
+    private Button uploadIMG;
 
-    String title, author, publisher, pubdate, genre;
-    int ISBN, edition;
+    @FXML
+    private TextField userIDTF;
 
-    private ObservableList<Book> listM = null;
+    @FXML
+    void add(ActionEvent event) {
 
-    public void initialize(URL url, ResourceBundle rb) {
+    }
 
+    @FXML
+    void addBookCopy(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deletebookCopy(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rentBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reserveRoom(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void send(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sendReminder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void update(ActionEvent event) {
+
+    }
+
+    @FXML
+    void upload(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
         col_ISBN.setCellValueFactory(new PropertyValueFactory<>("ISBN"));
         col_title.setCellValueFactory(new PropertyValueFactory<>("title"));
-        col_author.setCellValueFactory(new PropertyValueFactory<>("author"));
-        col_publisher.setCellValueFactory(new PropertyValueFactory<>("publisher"));
-        col_edition.setCellValueFactory(new PropertyValueFactory<>("edition"));
-        col_pubdate.setCellValueFactory(new PropertyValueFactory<>("pubdate"));
-        col_genre1.setCellValueFactory(new PropertyValueFactory<>("genre1"));
-        listM = Book.getStaffDataBookCopies();
-        tableBooks.setItems(listM);
+        col_author.setCellValueFactory(new PropertyValueFactory<>("authorFullName"));
+        col_copyNo.setCellValueFactory(new PropertyValueFactory<>("copyNumber"));
+        ObservableList<Book> listBooks = Book.getStaffDataBookCopies();
+        tableBooks.setItems(listBooks);
+
+        col_reservationID.setCellValueFactory(new PropertyValueFactory<>("reservationId"));
+        col_RoomID.setCellValueFactory(new PropertyValueFactory<>("studyRoomId"));
+        col_userID.setCellValueFactory(new PropertyValueFactory<>("userId"));
+        col_reservationDateTime.setCellValueFactory(new PropertyValueFactory<>("reservationDateTime"));
+        col_reservationEndTime.setCellValueFactory(new PropertyValueFactory<>("reservationEndDateTime"));
+        ObservableList<RoomsReservation> listReservations = null; //TO BE CHANGED!!!!!!!!!!!!! TO BE CHANGED!!!!!!!!!!!!!!!
+        tableReservations.setItems(listReservations);
+
 
     }
-    void setVariables(){
-        this.author = AuthorTF.getText();
-        this.edition = Integer.parseInt(editionTF.getText());
-        this.genre = genreTF.getText();
-        this.ISBN = Integer.parseInt(ISBNTF.getText());
-        this.publisher = publisherTF.getText();
-        this.pubdate = pubDateTF.getText();
-        this.title = titleTF.getText();
-    }
-
-    @FXML
-    void add(ActionEvent event) throws SQLException {
-
-        setVariables();
-        add_book( ISBN, title,  author,
-                publisher,edition ,  pubdate, genre);
-    }
-
-    @FXML
-    void delete(ActionEvent event) throws SQLException {
-        this.ISBN = Integer.parseInt(ISBNTF.getText());
-
-        delete_book( ISBN);
-
-    }
-
-    @FXML
-    void update(ActionEvent event) throws SQLException {
-
-        setVariables();
-        update_book( ISBN, title,  author,
-                publisher,edition ,  pubdate, genre);
-    }
-
 }
